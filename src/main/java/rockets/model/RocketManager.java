24f3a8dbb66acccf0a17c006ced9a0cdc9c7df5d
Veka <502114303@qq.com>
2019-04-29 00:20:15 +1000

    Adding get set methods and validations to RocketManager

--- src/main/java/rockets/model/RocketManager.java
+++ src/main/java/rockets/model/RocketManager.java
@@ -39,6 +39,18 @@ public class RocketManager extends Entity {
     {
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        notBlank(password, "password cannot be null or empty");
+        if (isValidPassword(password) == true)
+            this.password = password;
+        else
+            throw new IllegalArgumentException("Password should be 6 to 20 characters and contain at least one upper and lower case");
+    }
+
     public String getFirstName() {
         return firstName;
     }
@@ -51,6 +63,28 @@ public class RocketManager extends Entity {
             throw new IllegalArgumentException("FirstName should be letters and not exceed 20");
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        notBlank(email, "email cannot be null or empty");
+        if (isValidEmailAddress(email) == true)
+            this.email = email;
+        else
+            throw new IllegalArgumentException("Email should be a valid email address");
+    }
+
+    public boolean isValidEmailAddress(String email)
+    {
+        boolean emailValidation = false;
+        if(email.matches("^(.+)@(.+)$"))
+            emailValidation = true;
+        else
+            System.out.println("The email address is invalid");
+        return emailValidation;
+    }
+
     public String getLastName() { return lastName; }
 
     public void setLastName(String lastName)
@@ -61,4 +95,21 @@ public class RocketManager extends Entity {
         else
             throw new IllegalArgumentException("LastName should be letters and not exceed 20");
     }
+
+    public boolean isValidLastName(String lastName)
+    {
+        boolean lastNameValidation = false;
+        if(lastName.matches("[a-zA-Z]*"))
+        {
+            if (lastName.length() <= 20)
+                lastNameValidation = true;
+            else
+                System.out.println("The length of last name shouldn't exceed 20 letters");
+        }
+        else
+            System.out.println("The last name should only contain letters");
+        return lastNameValidation;
+    }
+
+
 }
